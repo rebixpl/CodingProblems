71c0ea10ca6eb43a19f1ba52f28d63724a3e9766
rebixpl <33410494+rebixpl@users.noreply.github.com>
2022-08-20 10:43:05 +0200

    Create 27-remove-element.js

---
+++ LeetCode-JS/27-remove-element.js
@@ -0,0 +1,33 @@
+/* Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+The relative order of the elements may be changed.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the 
+result be placed in the first part of the array nums. More formally, if there are k elements after 
+removing the duplicates, then the first k elements of nums should hold the final result. It does not 
+matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place 
+with O(1) extra memory.
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+function removeElement(nums, val) {
+  if (!nums) return 0;
+  let pointer = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[pointer] = nums[i];
+      pointer++;
+    }
+  }
+  return pointer;
+}
+
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)); // 5, [ 0, 1, 3, 0, 4, _, _, _ ]
